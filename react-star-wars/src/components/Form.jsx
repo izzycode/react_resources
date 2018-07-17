adc7588491357bba85fd5cc35026ab89d1676415
IzzyCode <israel@izzycode.co>
2018-07-17 17:32:22 -0400

    Add React Star Wars app

---
+++ react-star-wars/src/components/Form.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import '../css/Form.css'
+
+class Form extends Component {
+  constructor(){
+    super()
+    this.state = {
+      selection: {},
+      characters: [],
+    }
+
+    this.displayOptions = this.displayOptions.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSelect = this.handleSelect.bind(this)
+    this.newUrl = this.newUrl.bind(this)
+  }
+
+  displayOptions(){
+    return this.state.characters.map( (char, indx) => {
+      return <option key={indx} value={indx}> {char.name}</option>
+    })
+  }
+  
+  handleSubmit(ev){
+    ev.preventDefault()
+    let url = this.newUrl()
+    this.props.history.push( this.state.selection.name.replace(' ','_') )
+  }
+
+  handleSelect(ev){
+    let indx = ev.target.value
+    let newChar = this.state.characters[indx]
+    this.setState({ selection: newChar })
+  }
+
+  componentDidMount(){
+    fetch('https://swapi.co/api/people')
+      .then( res => res.json() )
+      .then( res => {
+        this.setState({
+          characters: res.results,
+        })
+      })
+  }
+
+  render(){
+    return(
+      <div className="sw-select">
+        <h1>Lookup any Star Wars Character!</h1>
+        <form id="sw-form" onSubmit={this.handleSubmit}>
+          <select name="characters" onChange={this.handleSelect}>
+            <option value="---">Choose a character</option>
+            { this.displayOptions() }
+          </select>
+          <button type="submit">Go</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Form
---
+++ react-star-wars/src/components/Main.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Form from './Form'
+import Profile from './Profile'
+import { BrowserRouter,  Route } from  'react-router-dom'
+
+import '../css/Main.css'
+
+class Main extends Component {
+  render(){
+    return(
+      <div className="star-wars">
+        <header>
+          <img src="https://i.imgur.com/SuvBCb2.png" alt="star wars logo" />
+        </header>
+        <main>
+          <BrowserRouter>
+            <div>
+              <Route path="/" component={Form} />
+              <Route
+                path="/:character_id"
+                render={ props => {
+                  return (<Profile {...props}
+                    key={props.match.params.character_id}  />
+                  )
+                }
+              } />
+            </div>
+          </BrowserRouter>
+        </main>
+      </div>
+    )
+  }
+}
+
+export default Main
